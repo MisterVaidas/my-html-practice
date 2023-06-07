a41826729396b368e17465e521bcbd6ced821a80
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-06-07 19:37:34 +0100

    updated files

---
+++ navButtons.js
@@ -0,0 +1,19 @@
+const navButtons = document.querySelectorAll('.header nav a');
+navButtons.forEach(button => {
+  button.addEventListener('mouseenter', () => {
+      button.style.backgroundColor = 'light blue';
+      button.style.color = '#00FF00';
+      button.style.width = '115px';
+      button.style.heigth = '5px';
+      button.style.border = '2px solid red'
+      button.style.borderRadius = '50px';
+      button.font.size = "30px";
+  });
+
+  button.addEventListener('mouseleave', () => {
+      button.style.backgroundColor = 'transparent';
+      button.style.color = 'White';
+      button.style.border = 'none';
+      
+  });
+});
---
+++ script.js
@@ -0,0 +1,58 @@
+let myName = "Vaidas";
+console.log(myName);
+
+let myPet = "Dog" // this data type is a string
+let myPetAge = 7; // this data type is a number
+let petDetails = "My Pet is " + myPetAge
+console.log(petDetails);
+console.log(typeof petDetails);
+
+let likesWalkies = true // this data type is a boolean
+console.log(typeof likesWalkies);
+
+let addition = 7 + 7
+console.log(addition);
+
+let subtraction = 15 - 10
+console.log(subtraction);
+
+let multiplication = 8 * 10
+console.log(multiplication);
+
+let division = 90 / 3
+console.log(division);
+
+let myNum = 5;
+let thisNum = ++myNum;
+console.log(thisNum);
+
+let thatNum = --myNum;
+console.log(thatNum);
+
+if (myNum === 5) {
+    console.log("Bingo");
+}
+
+if (myNum === "5") {
+    console.log("flamingo");
+}
+
+if (myNum === 6) {
+    console.log("You won!");
+} else {
+    console.log("Try again");
+}
+
+if (myNum === 10) {
+    console.log("Run the program");
+} else if (myNum === 3) {
+    console.log("Run this program");
+} else {
+    console.log("Do not run the program")
+}
+
+//let userName = prompt("Hi user, please tell me your name");
+//console.log(userName);
+
+//const welcomeMsg = alert("Welcome to my page" + userName);
+
